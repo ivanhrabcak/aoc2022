a61379b92905da67195ef787320f4865260408b0
Ivan Hrabcak <ivan@hrabcak.eu>
2022-12-05 18:15:34 +0100

    Refactor everything into 'one-liners'

--- day5/day5.js
+++ day5/day5.js
@@ -1,74 +1,74 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const input = fs.readFileSync('input.txt').toString().split('\n');
+const input = fs.readFileSync('input.txt').toString().split('\n')
 
 const parseInput = (x) => {
-    let ship = [[], [], [], [], [], [], [], [], []];
-
-    let i = 0;
-    let line;
-    while ((line = x[i++]).includes('[')) {
-        // 🤭
-        const crates = line
-            .slice(1)
-            .replace(/]/g, '] [')
-            .replace(/    /g, '] [')
-            .split('] [')
-            .map(x => x.replace('[', '').replace(' ', ''))
-            .slice(0, 9);
-
-        crates.forEach((crate, i) => ship[i].push(crate));
-    }
-
-    ship = ship.map((x) => x.reverse().filter(x => x !== ''));
-    return ship;
+    return x.slice(0, 8)
+        .map((x) => [...x].filter((_, i) => i != 0 && (i - 1) % 4 == 0))
+        .reduce(
+            (acc, x) => {
+                x.forEach((k, i) => acc[i].push(k))
+                
+                return acc
+            }, 
+            new Array(
+                (x[0].length + 1) / 4
+            ).fill(1).map(() => [])
+        )
+        .map(x => x.filter(k => k !== ' ').reverse())
 }
 
 const part1 = (input) => {
-    const ship = parseInput(input);
+    const ship = parseInput(input)
 
-    let i = 9;
-    while (++i != input.length) {
-        const [n, from, to] = input[i]
-            .replace('move ', '')
+    const result = input.slice(10)
+        .map((x) => (
+            x.replace('move ', '')
             .replace(' from ', ' ')
             .replace(' to ', ' ')
             .split(' ')
-            .map(x => parseInt(x));
+            .map((x, i) => i !== 0 ? parseInt(x) - 1 : parseInt(x))
+        ))
+        .reduce((s, [n, from, to]) => {
+            for (let i = 0; i < n; i++) {
+                const crate = s[from].pop()
+                s[to].push(crate)
+            }
 
-        for (let i = 0; i < n; i++) {
-            const crate = ship[from - 1].pop();
-            ship[to - 1].push(crate)
-        }
-    }
+            return s
+        }, ship)
+        .map(x => x.pop())
+        .join('')
 
-    const result = ship.map(x => x.pop()).join('');
-    console.log(result);
+    console.log(result)
 }
 
 const part2 = (input) => {
-    const ship = parseInput(input);
+    const ship = parseInput(input)
 
-    let i = 9;
-    while (++i != input.length) {
-        const [n, from, to] = input[i]
-            .replace('move ', '')
+    const result = input.slice(10)
+        .map((x) => (
+            x.replace('move ', '')
             .replace(' from ', ' ')
             .replace(' to ', ' ')
             .split(' ')
-            .map(x => parseInt(x));
+            .map((x, i) => i !== 0 ? parseInt(x) - 1 : parseInt(x))
+        ))
+        .reduce((s, [n, from, to]) => {
+            const stack = s[from]
+            const stackTaken = stack.slice(stack.length - n, stack.length)
+            const stackLeft = stack.slice(0, stack.length - n)
 
-        const stack = ship[from - 1];
-        const stackTaken = stack.slice(stack.length - n, stack.length);
-        const stackLeft = stack.slice(0, stack.length - n);
+            s[from] = stackLeft
+            stackTaken.forEach(x => s[to].push(x))
 
-        ship[from - 1] = stackLeft;
-        stackTaken.forEach(x => ship[to - 1].push(x));
-    }
+            return s
+        }, ship)
+        .map(x => x.pop())
+        .join('')
 
-    const result = ship.map(x => x.pop()).join('');
-    console.log(result);   
+    console.log(result)
 }
 
-part1(input);
-part2(input);
+part1(input)
+part2(input)
